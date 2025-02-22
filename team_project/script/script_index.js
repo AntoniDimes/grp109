5b6c4ac7ce80dd00fc231c1404e600b61592494f
kaurgur13 <gurkiratk7@icloud.com>
2025-02-21 16:07:15 -0800

    Update script_index.js
    Added variables for a typing speed test, including a time limit, score tracking, and an array of sentences.
    
    Added variables, array, and DOM (document.getElementById())

--- team_project/script/script_index.js
+++ team_project/script/script_index.js
@@ -1,1 +1,21 @@
 
+//Gurkirat 
+let startTime = 0; //start rtime
+let typedCorrect = 0; //score
+let mistakes = 0;
+let timer;
+let timeLimit = 30; // Set a time limit for the test (30 seconds)
+
+let sentences = [
+    "This is a test",
+    "This is another test" //to add more sentences to array
+];
+
+//DOM elements
+let sentenceDisplayed = document.getElementById('sentence'); //display sentence to type
+let userInput = document.getElementById('input');//user input
+let timeDisplayed = document.getElementById('time');//displays time
+let mistakeCounter = document.getElementById('mistakes');//
+let results = document.getElementById('result');//displays result
+
+
