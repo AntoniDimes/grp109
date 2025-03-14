ced8cc60cd9973bbe5574d1656524f62e054a0fa
kaurgur13 <gurkiratk7@icloud.com>
2025-03-13 18:22:12 -0700

    Create carousel.js
    added array of images and created file

---
+++ team_project/js/carousel.js
@@ -0,0 +1,8 @@
+const images = [ // Array of images (gurkirat)
+  { src: "image/keyboards.png", alt: "Image of a keyboard" },
+  { src: "image/keyboards2.png", alt: "Image of a keyboard" },
+  { src: "image/keyboards4.png", alt: "Image of a keyboard" },
+  { src: "image/keyboards5.png", alt: "Image of a keyboard" },
+  { src: "image/keyboards6.png", alt: "Image of a keyboard" },
+  { src: "image/keyboards7.png", alt: "Image of a keyboard" }
+];
